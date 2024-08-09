88e510b57d0e285497c4ba11039c98547eac58a0
Ubuntu <ubuntu@ip-172-31-16-40.us-east-2.compute.internal>
2024-08-09 19:10:29 +0000

    typo

--- src/test/java/com/example/ezlearn/EzLearnAcademyApplicationTests.java
+++ src/test/java/com/example/ezlearn/EzLearnAcademyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ezlearn;
+package com.example.ezlearnacademy; // Update this to match the main application class
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
