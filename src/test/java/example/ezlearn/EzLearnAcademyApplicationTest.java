b06df866a88204b76b3be3062861d0a8b2f7899e
Ubuntu <admin@ymail.com>
2024-08-09 16:42:37 +0000

    more changes

---
+++ src/test/java/example/ezlearn/EzLearnAcademyApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.ezlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EzLearnAcademyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
